426d2782c391d66779e05dc8079f998db99afe65
cosimogiove <cosimo18497@gmail.com>
2024-02-15 18:43:45 +0100

    feat: esercizio terminato

---
+++ my-vue-app/src/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+    name:"mainheader",
+}
+</script>
+
+<template>
+<header>
+    <h1>
+        il mio portfolio:
+    </h1>
+</header>
+</template>
+
+<style scoped>
+h1{
+    text-align: center;
+    color: black;
+    padding: 20px;
+}
+
+header{
+    background-color:lightgrey;
+}
+</style>
---
+++ my-vue-app/src/AppMain.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "app main",
+    props: ["projects"]
+}
+</script>
+
+<template>
+    <main>
+        <div class="container-card container d-flex">
+            <div class="card mb-3" v-for="project in projects" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <h2>titolo:</h2>
+                        {{ project.title }}</h5>
+                    <p class="card-text">
+                        <h2>descrizione:</h2>
+                        <div>
+                            {{ project.description }}
+                        </div>
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.container-card{
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+}
+
+
+</style>
